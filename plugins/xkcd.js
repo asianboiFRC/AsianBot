875634dbad2494e27f94058fc902cd318e455365
Michael Cao <mike.x.cao@gmail.com>
2016-10-02 14:04:14 -0400

    Added XKCD Functionality

--- plugins/xkcd.js
+++ plugins/xkcd.js
@@ -1,11 +1,39 @@
 module.exports = {
 	main: function(bot, message) {
-		var xkcd = require('xkcd-imgs');
-		xkcd.img(function(err, res){
-			if(!err) {
-				message.channel.sendMessage(res.title + "\n" + res.url);
-				console.log(res);
-			}
-		});
+		var xkcd = require('xkcd');
+		
+		if(message.content == "frc") {
+			xkcd(689, function (err, res) {
+				if(!err) {
+					message.channel.sendMessage("**" + res.num + "**: " + res.title + "\n" + res.alt + "\n*" + res.alt);
+					console.log(data);
+				}
+				
+				if(err) {
+					message.channel.sendMessage("Error retrieving XKCD.");
+				}
+			});
+		}
+		
+		if(message.content != "~xkcd") {
+			xkcd(message.content, function (err, res) {
+				if(!err) {
+					message.channel.sendMessage("**" + res.num + "**: " + res.title + "\n" + res.alt + "\n*" + res.alt);
+					console.log(data);
+				}
+				
+				if(err) {
+					message.channel.sendMessage("Error retrieving XKCD.");
+				}
+			});
+		}
+		if(message.content == "~xkcd"){
+			xkcd(function (err, res) {
+				if(!err) {
+					message.channel.sendMessage("**" + res.num + "**: " + res.title + "\n" + res.alt + "\n*" + res.alt);
+					console.log(data);
+				}
+			});
+		}
 	}
 };
