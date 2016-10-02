b5e25ab66689ad9588a072db1df9bb85255b7dee
Michael Cao <mike.x.cao@gmail.com>
2016-10-02 10:55:05 -0400

    v2.6.5 Bot fixes

--- plugins/take.js
+++ plugins/take.js
@@ -1,5 +1,6 @@
 module.exports = {
 	main: function(bot, message) {
+		const isCommander = ["171319044715053057", "180094452860321793"];
 		if (message.member.roles.exists('name', 'Bot Commander') || isCommander.indexOf(message.author.id) > -1){
 			var user = message.mentions.users.array()[0];
 			var roleToTake = message.content.split(" ").splice(1).join(" ");
