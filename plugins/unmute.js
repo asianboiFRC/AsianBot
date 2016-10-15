ef169367c9aa8c08749b911bcd16d58fbf94903f
Michael Cao <Michael Cao>
2016-10-15 16:03:02 -0400

    mute fix

--- plugins/unmute.js
+++ plugins/unmute.js
@@ -4,11 +4,12 @@ module.exports = {
 		if (msg.member.roles.exists('name', 'Bot Commander')) {
 			try {
 				var muted = msg.guild.members.find('id', mutee.id);
-				let role = message.guild.roles.find("name", 'muted');
+				let role = message.guild.roles.find('name', 'muted');
 				muted.removeRole(role).catch(console.error);
 				msg.reply(mutee + ' has been unmuted.');
 			} catch (e) {
-				msg.channel.sendMessage('Muted Role does not exist');
+				console.log(e);
+				msg.channel.sendMessage('muted Role does not exist');
 			}
 		} else {
 			msg.reply('You do not have permission to do this action');
