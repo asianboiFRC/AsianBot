8121702a3aeda560b1377f285c655415f92197d5
Michael Cao <Michael Cao>
2016-10-16 10:35:17 -0400

    Fixed prefix again

--- plugins/prefix.js
+++ plugins/prefix.js
@@ -13,11 +13,11 @@ module.exports = {
 		const owner = bot.users.get(config.owner);
 		
 		if(message.author.id == message.guild.owner.id || message.author.id == config.owner) {
-			connection.query('SELECT DISTINCT prefix FROM servers WHERE id = ' + msg.guild.id, function (error, results, fields) {
+			connection.query('SELECT DISTINCT prefix FROM servers WHERE id = ' + message.guild.id, function (error, results, fields) {
 				var PREFIX = results[0].prefix;
 				var newprefix = message.content.substring(0, 1);
 				if(PREFIX == newprefix) {
-					msg.reply("this is the same prefix!");
+					message.reply("this is the same prefix!");
 					return;
 				}
 				
