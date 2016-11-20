4c230462fe254b89e607bc2f2c5699add99d67bf
Michael Cao <mike.x.cao@gmail.com>
2016-11-19 22:20:29 -0500

    updates

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,9 +1,9 @@
 module.exports = {
 	main: function(bot, message) {
-		var start = new Date(message.timestamp).getTime();
+		var start = new Date(message.createdAt).getTime();
 
 		message.channel.sendMessage("Pong!")
-			.then(message => message.edit("Hello, pong! You're on the " + message.channel.guild.name + " server.\nTook " + (message.timestamp.getTime() - start) + " ms to respond."))
+			.then(message => message.edit("Hello, pong! You're on the " + message.channel.guild.name + " server.\nTook " + (message.createdAt.getTime() - start) + " ms to respond."))
 			.catch(console.error);
 	}
 };
