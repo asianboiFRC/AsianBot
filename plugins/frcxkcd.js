446e9155f2c7bbb485df8062c6b6f86b4ac5155f
Michael Cao <mike.x.cao@gmail.com>
2016-12-06 19:55:16 -0500

    XKCD improvements

--- plugins/frcxkcd.js
+++ plugins/frcxkcd.js
@@ -2,7 +2,7 @@ module.exports = {
 	main: function(bot, message) {
 		var xkcd = require('xkcd');
 			
-		xkcd(function (data) {
+		xkcd(689, function (data) {
 			message.channel.sendMessage("**" + data.num + "**: " + data.title + "\n" + data.alt + "\n*" + data.img);
 			console.log(data);
 		});
